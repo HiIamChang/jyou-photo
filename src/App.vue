3535b608cc1f887272864a4017dbf9ce339f46d5
我是常佳奇 <124332330+HiIamChang@users.noreply.github.com>
2023-10-12 16:15:51 +0800

    element-ui imported

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue jyou-logo" src="./assets/jyou-logo.png">
     <HelloWorld msg="Welcome , jyou's first website is building ..."/>
+    <el-button @click="startHacking">Start</el-button>
   </div>
 </template>
 
@@ -12,17 +13,28 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'Imported successfully',
+        type: 'success',
+        message: 'If u can see it , it means element-ui already been imported.',
+        duration: 5000
+      })
+    }
   }
 }
 </script>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica,  sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
 }
 </style>
